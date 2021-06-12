12e483ca68f11285943c084cf9430cc6be528070
lmz <mingzeluo888@gmail.com>
2021-06-12 10:41:32 +0800

    a

--- src/main/java/com/example/ordersystem/mapper/FoodMapper.java
+++ src/main/java/com/example/ordersystem/mapper/FoodMapper.java
@@ -7,7 +7,8 @@ import java.util.List;
 
 @Mapper
 public interface FoodMapper {
-    @Select("select * from food")
+
+    /*@Select("select * from food")
     @Results({
             @Result(column = "f_id", property = "id"),
             @Result(column = "f_name", property = "name"),
@@ -15,21 +16,14 @@ public interface FoodMapper {
             @Result(column = "f_price", property = "price"),
             @Result(column = "f_type", property = "type"),
             @Result(column = "f_img", property = "img")
-
-    })
+    })*/
     List<Food> selectFood();
 
-    @Insert("insert into food(f_id, f_name, f_materials, f_price, f_type, f_img) " +
-            "values(#{id}, #{name}, #{material}, #{price}, #{type}, #{img})")
     boolean insertFood(Food food);
 
-    @Select("select f_id from food")
     List<Integer> getF_ids();
 
-    @Update("update food set f_name=#{name}, f_materials=#{material}, f_price=#{price}, f_type=#{type}" +
-            " where f_id=#{id}")
     boolean updateFoodById(Food food);
 
-    @Delete("delete from food where f_id=#{id}")
     boolean deleteFoodById(Food food);
 }
--- src/test/java/com/example/ordersystem/OrderSystemApplicationTests.java
+++ src/test/java/com/example/ordersystem/OrderSystemApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.ordersystem;
 
+import com.example.ordersystem.mapper.FoodMapper;
+import com.example.ordersystem.pojo.Food;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class OrderSystemApplicationTests {
+    @Autowired
+    private FoodMapper foodMapper;
 
     @Test
     void contextLoads() {
+        List<Food> foods=foodMapper.selectFood();
+        for (Food food: foods) {
+            System.out.println(food.getId()+" , "+food.getName()+" , "+
+            food.getMaterial()+" , "+food.getPrice()+" , "+food.getType());
+        }
+//        List<Integer> f_ids = foodMapper.getF_ids();
+//        for (int id: f_ids) {
+//            System.out.println(id);
+//        }
     }
 
 }
